310d798dec3a07fff943b4736682081362aad9d0
Rukosa <mwmcginnis490@gmail.com>
2022-03-16 14:47:15 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-Drawing/MICHAELMCGINNIS_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_DrawingApp.swift
+//  MICHAELMCGINNIS-Drawing
+//
+//  Created by Michael Mcginnis on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
